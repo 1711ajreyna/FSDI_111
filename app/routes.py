d04eab6f74ff2135dc73b5a83c9f61656a4fb5be
Andrew Reyna <1711ajreyna@gmail.com>
2024-04-17 17:39:51 -0700

    routes.py changes

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask         # from the flask module import the Flask class
 
 app = Flask(__name__)           # Create an instance of the Flask class
 
+@app.get("/about")
 @app.get("/")                   # Flask decorater that maps view functions to routes
 def index():                    # view function
     me = {                      #python dictionary
